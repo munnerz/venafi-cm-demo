748c5bd58f02f4eb39c3912f7e0e0e12982a2128
James Munnelly <james@munnelly.eu>
2019-02-05 11:41:23 +0000

    Add script and README
    Signed-off-by: James Munnelly <james@munnelly.eu>

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # venafi-cm-demo
-Venafi/cert-manager demo repository
+
+This repository contains a script that can be used to quickly provision
+a demo environment to show cert-manager with Venafi integration.
+
+It is compatible with Linux and OSX, and only requires Docker in order
+to run.
+
+Any additional required dependencies will be pulled automatically.
+
+## Usage
+
+To get started, simply run:
+
+```bash
+$ ./setup-demo.sh
+
+...
+
+All components have been deployed!
+
+Please run the following command to make 'kubectl' work:
+
+	export KUBECONFIG=/Users/james/.kube/kind-config-venafi
+```
+
+It may take up to 2 minutes to provision the environment.
+
+## Issues
+
+If you encounter any issues whilst running this script, please
+open an issue on this repository.
+
---
+++ start-demo.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+bin="/usr/local/bin"
+SCRIPT_ROOT="$(cd "$(dirname "$0")" && pwd -P)"
+
+echo "+++ Grabbing tools needed for cert-manager demo"
+
+echo "+++ Fetching 'kind'"
+if [[ ! -f "${bin}/kind" ]]; then
+  if [[ "$(uname)" == "Darwin" ]]; then
+    curl -Lo "${bin}/kind" https://github.com/kubernetes-sigs/kind/releases/download/0.1.0/kind-darwin-amd64
+  elif [[ "$(uname)" == "Linux" ]]; then
+    curl -Lo "${bin}/kind" https://github.com/kubernetes-sigs/kind/releases/download/0.1.0/kind-linux-amd64
+  else
+    echo "unsupported platform $(uname)"
+    exit 1
+  fi
+else
+  echo "Found existing copy of kind, skipping download."
+fi
+chmod +x "${bin}/kind"
+
+echo "+++ Fetching 'kubectl'"
+if [[ ! -f "${bin}/kubectl" ]]; then
+  if [[ "$(uname)" == "Darwin" ]]; then
+    curl -Lo "${bin}/kubectl" https://storage.googleapis.com/kubernetes-release/release/v1.13.2/bin/darwin/amd64/kubectl
+  elif [[ "$(uname)" == "Linux" ]]; then
+    curl -Lo "${bin}/kubectl" https://storage.googleapis.com/kubernetes-release/release/v1.13.2/bin/linux/amd64/kubectl
+  else
+    echo "unsupported platform $(uname)"
+    exit 1
+  fi
+else
+  echo "Found existing copy of kubectl, skipping download."
+fi
+chmod +x "${bin}/kubectl"
+
+echo "+++ Grabbed tools"
+export PATH="${bin}:$PATH"
+
+unset KUBECONFIG
+kind delete cluster --name venafi > /dev/null 2>&1 || true
+
+echo "+++ Creating local Kubernetes cluster..."
+CLUSTER_NAME="venafi"
+if ! kind create cluster --image kindest/node:v1.13.2 --name "$CLUSTER_NAME"; then
+    echo "Creating Kubernetes cluster named $CLUSTER_NAME failed."
+    echo "If the error above complains that a docker container already exists,"
+    echo "please run '${bin}/kind delete cluster --name $CLUSTER_NAME'"
+    exit 1
+fi
+echo "+++ Created local Kubernetes cluster"
+
+export KUBECONFIG="$(kind get kubeconfig-path --name "$CLUSTER_NAME")"
+echo "+++ Using ${KUBECONFIG} to talk to local Kubernetes cluster"
+
+echo "+++ Verifying kubernetes apiserver is available"
+kubectl get nodes
+
+echo "+++ Deploying cert-manager"
+kubectl apply -f "${SCRIPT_ROOT}/cert-manager.yaml"
+
+echo
+echo
+echo -e "\e[32mAll components have been deployed!"
+echo
+echo -e "\e[32mPlease run the following command to make 'kubectl' work:"
+echo
+echo -e "\texport KUBECONFIG=$(kind get kubeconfig-path --name "$CLUSTER_NAME")"
+echo
